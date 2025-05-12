d7d396947983f11973bbfd7c79695789e2f45d30
conaN <dev@conan.moe>
2025-05-12 13:59:50 +0200

    update icon and emoji picker

--- src/rofi/emoji/emoji.fish
+++ src/rofi/emoji/emoji.fish
@@ -1,18 +1,23 @@
 #!/usr/bin/env fish
 
-argparse 'source=' 'theme=' 'fallback=' -- $argv 2>/dev/null
+set -l dir (status dirname)
 
-set -l content (cat $_flag_source)
-set -l selection (printf '%s\n' $content | rofi -dmenu -p '' -i -theme "$_flag_theme")
+set -l config "$dir/../config.yaml"
+set -l source "$dir/emoji.txt"
+
+set -l theme (yq -r '.variables.theme' "$config" | string replace '~' $HOME)
+set -l fallback (yq -r '.variables.fallback' "$config" | string replace '~' $HOME)
+
+set -l content (cat "$source")
+set -l selection (printf '%s\n' $content | rofi -dmenu -p '' -i -theme "$theme")
 
 if test -z "$selection"
-    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
+    test -n "$fallback" -a -e "$fallback"; and eval "$fallback" &
 else if contains -- "$selection" $content
     set -l emoji (string split -m1 ' ' -- $selection)[1]
     printf '%s' $emoji | wl-copy
     notify-send -t 2000 "Rofi" "$emoji copied to clipboard"
 else
     notify-send -u critical -t 2000 "Rofi" "⚠️ No match found"
-    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
+    test -n "$fallback" -a -e "$fallback"; and eval "$fallback" &
 end
-
