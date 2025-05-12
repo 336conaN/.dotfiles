2060213e55ed23a623904b2ef20ddf242c6ee54b
conaN <dev@conan.moe>
2025-05-12 12:38:08 +0200

    update icon and emoji picker

--- src/rofi/emoji/emoji.fish
+++ src/rofi/emoji/emoji.fish
@@ -3,20 +3,16 @@
 argparse 'source=' 'theme=' 'fallback=' -- $argv 2>/dev/null
 
 set -l content (cat $_flag_source)
-
 set -l selection (printf '%s\n' $content | rofi -dmenu -p '' -i -theme "$_flag_theme")
 
 if test -z "$selection"
-    if test -n "$_flag_fallback" -a -e "$_flag_fallback"
-        eval "$_flag_fallback" &
-    end
+    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
+else if contains -- "$selection" $content
+    set -l emoji (string split -m1 ' ' -- $selection)[1]
+    printf '%s' $emoji | wl-copy
+    notify-send -t 2000 "Rofi" "$emoji copied to clipboard"
 else
-    if string match -q -- "*$selection*" "$(printf '%s\n' $content)"
-        set -l emoji (string match -r '^\S+' $selection)
-        printf '%s' $emoji | wl-copy
-        notify-send -t 2000 "Rofi" "$emoji copied to clipboard"
-    else
-        notify-send -u critical -t 2000 "Rofi" "No matching emoji found"
-    end
+    notify-send -u critical -t 2000 "Rofi" "⚠️ No match found"
+    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
 end
 
--- src/rofi/icon/icon.fish
+++ src/rofi/icon/icon.fish
@@ -3,20 +3,16 @@
 argparse 'source=' 'theme=' 'fallback=' -- $argv 2>/dev/null
 
 set -l content (cat $_flag_source)
-
 set -l selection (printf '%s\n' $content | rofi -dmenu -p '' -i -theme "$_flag_theme")
 
 if test -z "$selection"
-    if test -n "$_flag_fallback" -a -e "$_flag_fallback"
-        eval "$_flag_fallback" &
-    end
+    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
+else if contains -- "$selection" $content
+    set -l icon (string split -m1 ' ' -- $selection)[1]
+    printf '%s' $icon | wl-copy
+    notify-send -t 2000 "Rofi" "$icon  copied to clipboard"
 else
-    if string match -q -- "*$selection*" "$(printf '%s\n' $content)"
-        set -l icon (string match -r '^\S+' $selection)
-        printf '%s' $icon | wl-copy
-        notify-send -t 2000 "Rofi" "$icon  copied to clipboard"
-    else
-        notify-send -u critical -t 2000 "Rofi" "No matching icon found"
-    end
+    notify-send -u critical -t 2000 "Rofi" "⚠️ No match found"
+    test -n "$_flag_fallback" -a -e "$_flag_fallback"; and eval "$_flag_fallback" &
 end
 
