a0c3e9b1d3caca0c030217874e057b7f3651248d
conaN <dev@conan.moe>
2025-05-12 23:47:09 +0200

    add screenshot and power menu

--- src/rofi/screenshot-menu/screenshot-menu.fish
+++ src/rofi/screenshot-menu/screenshot-menu.fish
@@ -4,18 +4,26 @@ set dir (status dirname)
 set font "SFMono Nerd Font"
 set theme "$dir/../theme/theme.rasi"
 set source "$dir/config.yaml"
+set fallback "$dir/../main/main.fish"
 
 set options (yq eval '.entries[] | "<span foreground=\"" + .color + "\" font_family=\"'$font'\">" + .icon + "</span>" + .title' $source)
 set actions (yq eval '.entries[].command' $source)
 
 set selection (printf '%s\n' $options | rofi -dmenu -p "" -theme "$theme" -i -matching fuzzy -markup-rows)
 
-if test -n "$selection"
+if test -z "$selection"
+    if test -n "$fallback" -a -e "$fallback"
+        eval "$fallback" &
+    end
+else
     set index (contains -i -- "$selection" $options)
     if test -n "$index"
         eval $actions[$index] &
     else
         notify-send -u critical -t 2000 "Rofi" "Invalid selection: $selection"
+        if test -n "$fallback" -a -e "$fallback"
+            eval "$fallback" &
+        end
     end
 end
 
